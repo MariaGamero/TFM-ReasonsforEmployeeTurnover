16f8988fff76856c52ae44a812b7319078fb94c7
María <37455195+MariaGamero@users.noreply.github.com>
2018-09-19 20:11:10 +0200

    Add files via upload

--- 2. Data_Mining.R
+++ 2. Data_Mining.R
@@ -28,29 +28,9 @@ summary(hr_data)
 ##### 4. Tratamiento de las variables #####
 hr_data_numericos=dummy.data.frame(hr_data, dummy.class="character" )
 
-## -------------------------------------------------------------------------
-##### 5. Bloque de Segmentación mediante Modelo RFM 12M  #####
-
-hr_dataScaled=scale(hr_data_numericos)
-
-#K-means is a simple unsupervised machine learning algorithm that groups a dataset 
-#into a user-specified number (k) of clusters.
-
-NUM_CLUSTERS=1
-set.seed(1234) #fijamos la semilla
-Modelo=kmeans(hr_dataScaled,NUM_CLUSTERS)
-
-hr_data$Segmentos=Modelo$cluster
-hr_data_numericos$Segmentos=Modelo$cluster
-
-table(hr_data_numericos$Segmentos)
-
-aggregate(hr_data_numericos, by = list(hr_data_numericos$Segmentos), mean)
-#nuestro employee medio es....
 
 ## -------------------------------------------------------------------------
-
-##### 6. Determine the optimal number of clusters (Elbow Method) #####
+##### 5. Determine the optimal number of clusters (Elbow Method) #####
 
 #One method to validate the number of clusters is the elbow method. 
 #The goal is to choose a small value of "k" that still has a low sum of squared errors, 
@@ -60,8 +40,27 @@ Intra <- (nrow(hr_data_numericos)-1)*sum(apply(hr_data_numericos,2,var))
 for (i in 2:15) Intra[i] <- sum(kmeans(hr_data_numericos, centers=i)$withinss)
 plot(1:15, Intra, type="b", xlab="Numero de Clusters", ylab="Suma de Errores intragrupo")
 
-#I can appreciate a pretty clear elbow at k = 3, indicating that 3 is the best number of clusters.
+#As can be seen from the graph, at k = 3 the elbow appears, indicating that 3 is the best number of clusters.
 
+
+## -------------------------------------------------------------------------
+##### 5. Segmentation mediante Modelo RFM 12M  #####
+
+hr_dataScaled=scale(hr_data_numericos)
+
+#K-means is a simple unsupervised machine learning algorithm that groups a dataset 
+#into a user-specified number (k) of clusters.
+
+NUM_CLUSTERS=3
+set.seed(1234)
+Modelo=kmeans(hr_dataScaled,NUM_CLUSTERS)
+
+hr_data$Segmentos=Modelo$cluster
+hr_data_numericos$Segmentos=Modelo$cluster
+
+table(hr_data_numericos$Segmentos)
+
+aggregate(hr_data_numericos, by = list(hr_data_numericos$Segmentos), mean)
 
 
 
